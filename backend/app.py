5282ddc777d81aa2bfbde3b5bc30fc98f71cb799
Pratham1209 <123623948+Pratham1209@users.noreply.github.com>
2024-12-23 19:06:11 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -90,5 +90,7 @@ def download_file():
     return send_file(destination_path, as_attachment=True, download_name="downloaded_file.xlsx")
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
