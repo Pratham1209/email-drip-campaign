240740cb9ff08670963d7b2f462afd0ae3f05e90
Pratham1209 <123623948+Pratham1209@users.noreply.github.com>
2024-12-23 17:07:06 +0530

    Update email_sender.py

--- backend/services/email_sender.py
+++ backend/services/email_sender.py
@@ -1,23 +1,24 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from config import GMAIL_EMAIL, GMAIL_PASSWORD  # Make sure to have your Gmail credentials here
-
+from dotenv import load_dotenv  # Make sure to have your Gmail credentials here
+import os
+load_dotenv()
 def send_email(subject, body, to_email):
     # Set up the server (Gmail SMTP)
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()  # Upgrade the connection to a secure encrypted SSL/TLS connection
     
     # Login to your email account
-    server.login(GMAIL_EMAIL, GMAIL_PASSWORD)
+    server.login(os.getenv("GMAIL_EMAIL"), os.getenv("GMAIL_PASSWORD"))
     
     # Prepare the email
     msg = MIMEMultipart()
-    msg['From'] = GMAIL_EMAIL
+    msg['From'] = os.getenv("GMAIL_EMAIL")
     msg['To'] = to_email
     msg['Subject'] = subject
     msg.attach(MIMEText(body, 'plain'))
     
     # Send the email
-    server.sendmail(GMAIL_EMAIL, to_email, msg.as_string())
+    server.sendmail(os.getenv("GMAIL_EMAIL"), to_email, msg.as_string())
     server.quit()
